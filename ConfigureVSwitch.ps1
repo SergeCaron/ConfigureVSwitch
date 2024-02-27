7ab14c19d1bfae92005777e241afb1a1257f5c92
SergeCaron <69397104+SergeCaron@users.noreply.github.com>
2024-02-27 11:21:18 -0500

    Initial release

---
+++ ConfigureVSwitch.ps1
@@ -0,0 +1,406 @@
+##******************************************************************
+## Revision date: 2024.02.22
+##
+##		2023.11.10: Proof of concept / Initial release
+##		2024.02.25: Get VLAN list from configuration file
+##
+## Copyright (c) 2023-2024 PC-Évolution enr.
+## This code is licensed under the GNU General Public License (GPL).
+##
+## THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+## ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+## IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+## PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+##
+##******************************************************************
+param (
+# Default Virtual Switch name
+	[parameter()]
+	[string]$DefaultManagementSwitch = 'ManagementSwitch',
+# Default Adapter Name of the Virtual Switch
+	[parameter()]
+	[string]$DefaultPhysicalPortName = '$Switch'
+)
+
+# PowerShell version number
+$RunTimeVersion = $PSVersionTable.PSVersion -join "."
+
+#$DefaultManagementSwitch = 'ManagementSwitch'
+#$DefaultPhysicalPortName = '$Switch'
+
+
+# Privilege Elevation Source Code: https://stackoverflow.com/questions/7690994/running-a-command-as-administrator-using-powershell
+
+# Get the ID and security principal of the current user account
+$myWindowsID = [System.Security.Principal.WindowsIdentity]::GetCurrent();
+$myWindowsPrincipal = New-Object System.Security.Principal.WindowsPrincipal($myWindowsID);
+
+# Get the security principal for the administrator role
+$adminRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator;
+
+# Check to see if we are currently running as an administrator
+if ($myWindowsPrincipal.IsInRole($adminRole))
+{
+    # We are running as an administrator
+    Clear-Host;
+	Write-Host "Create multiple VLANs for Windows 10/11"
+	Write-Host "---------------------------------------"
+	Write-Host
+	Write-Host "	(This script is based on an idea from Alexander Täffner"
+	Write-Host "	see https://taeffner.net/2022/04/multiple-vlans-windows-10-11-onboard-tools-hyper-v/)"
+	Write-Host
+	Write-Host "PowerShell version: ". $RunTimeVersion
+	Write-Host
+}
+else {
+    # We are not running as an administrator, so relaunch as administrator
+
+    # Create a new process object that starts PowerShell
+    $newProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell";
+
+    # Specify the current script path and name as a parameter with added scope and support for scripts with spaces in it's path
+    $newProcess.Arguments = "& '" + $script:MyInvocation.MyCommand.Path + "'"
+
+    # Indicate that the process should be elevated
+    $newProcess.Verb = "runas";
+
+    # Start the new process
+    [System.Diagnostics.Process]::Start($newProcess);
+
+    # Exit from the current, unelevated, process
+    Exit;
+}
+
+# Run your code that needs to be elevated here...
+
+# Check that minimal Hyper-V management features are installed
+
+Write-Host "Status of Hyper-V services on this system:"
+Write-Host "------------------------------------------"
+$HyperVState = Get-WindowsOptionalFeature -Online -FeatureName *hyper-v* | select DisplayName, FeatureName, State
+$HyperVState | ft * -AutoSize
+
+$HyperVEnabled = $True
+ForEach ( $Feature in @( "Microsoft-Hyper-V-Management-PowerShell", "Microsoft-Hyper-V-Services") )
+# Note: "Microsoft-Hyper-V-Services" is not a component in Windows Server
+  {
+    If ((Get-WindowsOptionalFeature -Online -FeatureName $Feature).State -eq "Disabled") `
+		{ Write-Warning "Windows feature $Feature must be installed before using this script."
+				$HyperVEnabled = $False
+		}
+  }
+If ($HyperVEnabled) `
+		{ Write-Warning "Note: This script will not attempt ANY recovery in case of errors." }
+Else	{	Pause
+			Exit 911 }
+
+Write-Host
+
+	# Get the filename containing the VLAN list, if any.
+	Add-Type -AssemblyName System.Windows.Forms
+	$FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{ 
+		# InitialDirectory = [Environment]::GetFolderPath('Desktop') 
+		InitialDirectory = $script:MyInvocation.MyCommand.Path
+		Filter = ''
+		Title = 'Please locate your list of VLANs'
+		}
+	$Void = $FileBrowser.ShowDialog()
+
+	# Get the list of VLANs, which may be a null string if it contains only comments or if the user aborts:
+	If ( ![string]::IsNullOrEmpty($FileBrowser.FileName)  )
+		 { $RequiredVLANs = (Get-Content $FileBrowser.FileName) -match "^[^#]" | ConvertFrom-String -PropertyNames ID, Name, Routes }
+	else { $RequiredVLANs = $Null }
+	# Make sure we have an array!
+	If ( $RequiredVLANs -eq $Null ) { $RequiredVLANs = @() }
+	elseif ($RequiredVLANs.GetType().Name -eq "PSCustomObject") { $RequiredVLANs = @( $RequiredVLANs ) }
+
+	# Add a default Untagged value
+	If (! ($RequiredVLANs.ID -contains 0) )
+		{ $RequiredVLANs += @( [PSCustomObject] @{ ID=0; Name="Untagged"; Routes="" } ) }
+
+	# Sanity check: we should only have VLAN ID, name, and optional routes
+	If ( ($RequiredVLANs | Get-Member -MemberType NoteProperty | Measure-Object).Count -ne 3) {
+		# We inherited some other porperty: don't trust this cinfiguration file.
+		Write-Warning $($FileBrowser.FileName + " is not properly formatted.")
+		Pause
+		Exit 911
+	  }
+
+	# Let the user select its choice of VLANs
+	$RequiredVLANs = $RequiredVLANs | Out-GridView -PassThru -Title "Please select ALL VLANs that should be managed on the target network interface:"
+	Write-Host "Configuration selected:"
+	Write-Host "-----------------------"
+	$RequiredVLANs | Out-Host
+	
+	# Parse the routes associated with these VLANs for VPN clients
+	$RequiredVLANs | Add-Member -Name "Routing" -MemberType NoteProperty -Value @()
+		# Property Routing is not displayed during the Grid-View selection
+	ForEach ($VLAN in $RequiredVLANs) {
+        If ( ![string]::IsNullOrEmpty($VLAN.Routes) ) {
+			$Routing = @()
+			$Networks = $VLAN.Routes -split ","	# Enumeration style (ne.tw.or.k/bits;ip.ad.dr.ess);(ne.tw.or.k/bits;ip.ad.dr.ess)...
+            $IgnoreNetworks = $False
+
+            ForEach ($Network in $Networks) {
+                Try {
+                        $Junk = $ErrorActionPreference # Tuck this away ;-)
+                        $ErrorActionPreference = "Stop"
+                        If ( $Network -match "^\((?<Prefix>[A-Za-z\d.:]+/\d+);(?<Gateway>[A-Za-z\d.:]+)\)$" ) {
+                            $Void = $($Matches.Prefix.Trim().Split("/"))
+                            If ([int] $Void[1] -le 64) {
+                                $Void = [IPAddress]$Void[0]
+                                $Void = [IPAddress]$Matches.Gateway.Trim()
+                            $Routing += [PSCustomObject] @{ Prefix=$Matches.Prefix; Gateway=$Matches.Gateway }
+                             }
+                            else { Throw }
+                          }
+                         else { Throw }
+                  }
+                Catch { Write-Warning $( $VLAN.Routes + " ignored for VLan id " + $VLAN.ID + ".")
+                        $IgnoreNetworks = $True
+                  }
+                Finally {
+                        $ErrorActionPreference = $Junk # Restore ...
+                  }
+              }
+
+			If ($IgnoreNetworks) { Write-Host "" }	# Only if warnings were issued
+			else { $VLAN.Routing = $Routing }		# Only if all routes appear valid (which may or may not be true ...)
+      }
+    }
+
+# Get all virtual switches
+[System.Object] $AllPhysicalPorts = Get-NetAdapter | Get-NetAdapterBinding | Where { ($_.Enabled -eq $true) -and ($_.ComponentID -eq "vms_pp") }
+If ($AllPhysicalPorts.ComponentID.Count -gt 0) {
+	$Caution = @"
+There are currently one or more virtual switches defind in this Hyper-V Configuration.
+It is recommended to remove this configuration before defning multiple VLANs for the management OS.
+Such removal may cause lost of connectivity to the network. The default route(s) will be displayed
+before and after removing any switch associated with a physical network adapter.
+
+Note that this script may hang if you redefine the same virtual switch without first deleting it.
+"@
+	Write-Warning $Caution
+
+	$AllPhysicalPorts | ft @{Label="Nom du port physique"; Expression={$_.Name}}, InterfaceDescription, Description -AutoSize
+	If ($(Read-Host "Enter 'Yes' to proceed, anything else to continue").tolower().StartsWith('yes')) {
+
+		# Display the default route(s) BEFORE removing the virtual switche(s).
+		# Unfortunately "Get-NetNeighbor -PolicyStore ActiveStore -State Reachable | ft ifIndex, IPAddress, LinkLayerAddress"
+		# is unreliable. Get default routes
+		$Foreground = $host.UI.RawUI.ForegroundColor
+		$host.UI.RawUI.ForegroundColor = "Green"
+
+		Write-Host
+		Write-Host "Current default route(s)"
+		Write-Host "------------------------"
+		Try { $DefaultRoutes = Get-NetRoute -DestinationPrefix "0.0.0.0/0" -PolicyStore ActiveStore -ErrorAction Stop
+			  $DefaultRoutes | ft InterfaceAlias, InterfaceIndex, NextHop }
+		Catch { $DefaultRoutes = $Null
+				Write-Host "None!" }
+
+		ForEach ($Gateway in $DefaultRoutes.NextHop) { If ( $(Test-NetConnection -ComputerName $Gateway -InformationLevel Quiet)) { Write-Host $Gateway, "is reachable." } }
+
+		$host.UI.RawUI.ForegroundColor = $Foreground
+
+		# Retirer tous les commutateurs virtuels associés
+		ForEach ($PhysicalPort in $AllPhysicalPorts) {
+
+			$Adapter = Get-NetAdapter -InterfaceDescription $PhysicalPort.InterfaceDescription
+			$Adapter | ft Name, PhysicalMediaType, LinkSpeed, MacAddress
+
+			$Switch = Get-VMSwitch -SwitchType External | Where { $_.NetAdapterInterfaceDescription -eq $PhysicalPort.InterfaceDescription }
+			$Switch | ft Name, SwitchType, NetAdapterInterfaceDescription
+
+
+			$DefinedVLANs = Get-VMNetworkAdapterVLAN -ManagementOS | Where { $_.ParentAdapter.SwitchName -eq $Switch.Name }
+
+					ForEach ($Port in $DefinedVLANs) {
+						$Junk = $( "Removal of VLAN " + $Port.AccessVlanId + " (" + $Port.ParentAdapter.Name + ")" )
+						If ( ($Port.OperationMode -eq 'Access') -or ($Port.OperationMode -eq 'Untagged') ) {
+							Remove-VMNetworkAdapter -ManagementOS -SwitchName $Switch.Name -Name $Port.ParentAdapter.Name
+							Write-Warning $( "Removal of VLAN " + $Port.AccessVlanId + " (" + $Port.ParentAdapter.Name + ")" )
+						  }
+					  }
+
+			Write-Warning $( "Removal of virtual switch " + $Switch.Name )
+			Remove-VMSwitch -Name $Switch.Name -Force
+
+			Rename-NetAdapter -InputObject $Adapter -NewName $( "Ethernet " + $Adapter.MacAddress.SubString(9) )
+		}
+
+		Write-Warning "Waiting 5 seconds for the system to identify the new interfaces..."
+		Start-Sleep -Seconds 5
+
+		$Foreground = $host.UI.RawUI.ForegroundColor
+		$host.UI.RawUI.ForegroundColor = "Green"
+
+		Write-Host
+		Write-Host "New default route(s)"
+		Write-Host "------------------------"
+		# Display the default route(s) AFTER removing the virtual switche(s).
+		# Unfortunately "Get-NetNeighbor -PolicyStore ActiveStore -State Reachable | ft ifIndex, IPAddress, LinkLayerAddress"
+		# is unreliable. Get default routes
+		Try { $DefaultRoutes = Get-NetRoute -DestinationPrefix "0.0.0.0/0" -PolicyStore ActiveStore -ErrorAction Stop
+			  $DefaultRoutes | ft InterfaceAlias, InterfaceIndex, NextHop }
+		Catch { $DefaultRoutes = $Null
+				Write-Host "None!" }
+
+		ForEach ($Gateway in $DefaultRoutes.NextHop) { If ( $(Test-NetConnection -ComputerName $Gateway -InformationLevel Quiet)) { Write-Host $Gateway, "is reachable." } }
+
+		$host.UI.RawUI.ForegroundColor = $Foreground
+
+	 }
+  }
+
+Write-Host
+
+If ($RequiredVLANs -ne $Null) {
+
+	# Get THE virtual switch (Only one switch is supported in this script).
+
+	  [System.Object] $Switch = Get-VMSwitch -SwitchType External
+	  If ($Switch.Count -eq 1)
+		{
+		  $Adapter = Get-NetAdapter -InterfaceDescription $Switch.NetAdapterInterfaceDescription
+		}
+	  ElseIf ($Switch.Count -eq 0)
+		{
+		  # Only Ethernet media is supported.
+		  [System.Object] $Adapter = (Get-NetAdapter | Where { $_.Status -eq "UP" -and $_.PhysicalMediaType -eq "802.3" })
+		  If ($Adapter.Name.Count -gt 1) 
+			{
+				  $Adapter = $Adapter | Out-GridView -PassThru -Title "Please select the target network interface:"
+			}
+		  If ($Adapter.Name.Count -eq 1)
+			{
+				[System.Object] $Switch = New-VMSwitch -Name "$DefaultManagementSwitch" -NetAdapterName $Adapter.Name # Implicit -SwitchType External
+				# Note: this also creates a untagged port (VLAN 0, even if there is no such thing ;-)
+			}
+		  Else {
+			  Write-Warning "This script supports the configuration of a single network adapter."
+			  Pause
+			  Exit 911
+			}
+		}
+	  Else {
+			  Write-Warning "This script supports a single virtual switch. You are welcomed to clean this configuration ;-)"
+			  Pause
+			  Exit 911
+		}
+
+	  Write-Host "VLANs for virtual switch :", $Switch.Name, "(Physical adapter:", $Adapter.Name, ")"
+	  Write-Host "----------------------------------------------------------------------------------"
+
+	  # Get the status of the switch on this system
+		$ManagedVLANs = @()
+		ForEach ( $Port in $(Get-VMNetworkAdapterVLAN -ManagementOS | Where { $_.ParentAdapter.SwitchName -eq $Switch.Name }) ){
+			$ManagedVLANs += New-Object -Type PSOBJECT -Property @{
+					Mode = $Port.OperationMode
+					VLANId = $Port.AccessVlanId
+					PortName = $Port.ParentAdapter.Name
+			}
+		  }
+
+	  # Add missing VLANs:
+		ForEach ($VLAN in $RequiredVLANs) {
+			If (-not ($VLAN.ID -in $ManagedVLANs.VLANId)) {
+				$ThisOne = Add-VMNetworkAdapter -ManagementOS -SwitchName $Switch.Name -Name $VLAN.Name -Passthru
+				If ($VLAN.ID -eq 0) { $ThisOne | Set-VMNetworkAdapterVLAN -Untagged }
+				Else { $ThisOne	| Set-VMNetworkAdapterVLAN -Access -VLANId $VLAN.ID }
+				Write-Host "Adding: ", $VLAN.ID, $VLAN.Name
+			  }
+			Else {	$Port = Get-VMNetworkAdapterVLAN -ManagementOS `
+							| Where { ($_.ParentAdapter.SwitchName -eq $Switch.Name ) -and `
+								(($_.OperationMode -eq "Access" -and $_.AccessVlanId -eq $VLAN.ID) -or ($_.OperationMode -eq "Untagged" -and $VLAN.ID -eq 0)) }
+					
+					If ( $Port.ParentAdapter.Name -ne $VLAN.Name) `
+						{ Rename-VMNetworkAdapter -ManagementOS -Name $Port.ParentAdapter.Name -NewName $VLAN.Name }
+			  }
+		  }
+
+	  # Remove what is no longer used: the Windows 10/11 GUI does not allow you to delete this type of interface
+		ForEach ($VLAN in $ManagedVLANs) {
+			If (($VLAN.Mode -eq "Access" -or $VLAN.Mode -eq "Untagged") -and -not ($VLAN.VLANId -in $RequiredVLANs.ID)) {
+				Remove-VMNetworkAdapter -ManagementOS -SwitchName $Switch.Name -Name $VLAN.PortName
+				Write-Host "Removing :", $VLAN.VLANId, $VLAN.PortName }
+		  }
+		
+		Write-Warning "Waiting 5 seconds for the system to identify the new interfaces..."
+		Start-Sleep -Seconds 5
+
+	  # Display virtual switch status
+		$ManagedVLANs = @()
+		ForEach ( $Port in $(Get-VMNetworkAdapterVLAN -ManagementOS | Where { $_.ParentAdapter.SwitchName -eq $Switch.Name }) ){
+			$ManagedVLANs += New-Object -Type PSOBJECT -Property @{
+					Mode = $Port.OperationMode
+					VLANId = $Port.AccessVlanId
+					PortName = $Port.ParentAdapter.Name
+					MACAddress = $(Get-VMNetworkAdapter -ManagementOS | Where { $_.Name -eq $Port.ParentAdapter.Name } ).MACAddress -replace '..(?!$)', '$&-'
+			}
+		  }
+		$ManagedVLANs | ft * -Autosize
+
+		# Disable all VLANs
+		ForEach ($VLAN in $ManagedVLANs) {
+			If ($VLAN.VLANId -ne 0) {
+				$Decorated = Get-NetAdapter | Where { $_.MACAddress -eq $VLAN.MACAddress } `
+						| Rename-NetAdapter -NewName $("Port " + $VLAN.PortName) -Passthru
+				
+				# Enable adapter to manage routes and remove existing routes
+				Enable-NetAdapter -InputObject $Decorated -Confirm:$false | Out-Null
+				Get-NetRoute -InterfaceIndex $Decorated.ifIndex | Remove-NetRoute -Confirm:$false
+
+				# Add default routes for VPN Clients
+				$Configuration = $RequiredVLANs | Where { $_.ID -eq $VLAN.VLANId }
+				ForEach ($Route in $Configuration.Routing) {
+					$NewRoute = New-NetRoute -DestinationPrefix $Route.Prefix -NextHop $Route.Gateway -InterfaceIndex $Decorated.ifIndex
+				 }
+
+				Disable-NetAdapter -InputObject $Decorated -Confirm:$false | Out-Null
+				Write-Host "VLAN ", $VLAN.VLANId, $("Port " + $VLAN.PortName), "is now disabled."
+			  }
+			Else {
+				# Beware: the untagged virtual interface has the same MAC address as the physical interface
+				$Decorated = Get-NetAdapter | Where { ($_.MACAddress -eq $VLAN.MACAddress) -and $_.InterfaceDescription.Contains("Hyper-V Virtual Ethernet Adapter") } `
+					| Rename-NetAdapter -NewName $($DefaultPhysicalPortName + " " + $VLAN.PortName)
+			  }
+		  }
+
+	  # Rename the physical port as needed
+	  Write-Host
+	  If ($Adapter.Name -ne $DefaultPhysicalPortName) { Rename-NetAdapter -InputObject $Adapter -NewName $DefaultPhysicalPortName }
+	  Write-Host "The network adapter has been renamed $DefaultPhysicalPortName."
+	  Write-Host
+  }
+
+Write-Host "Routes with a limited lifetime value:"
+Write-Host "-------------------------------------"
+$Done = $False
+Do {
+	Try {
+		# Wait a few seconds for the system to digest these routes. Why? I still don't know.
+		Start-Sleep -Seconds 5
+		# Get all routes for this interface
+		Get-NetRoute -IncludeAllCompartments -ErrorAction Stop `
+			| Where-Object -FilterScript { $_.ValidLifetime -ne ([TimeSpan]::MaxValue) } | fT ifIndex, DestinationPrefix, NextHop, InterfaceAlias, ValidLifetime -AutoSize
+		$Done = $True
+	  }
+	Catch {
+		Write-Warning "Waiting until routes are available again."
+	  }
+  } Until ($Done)
+
+Write-Host
+
+Write-Host "Please review the adapter metric for desired default route. It is typically 25 for a Gigabit adapter"
+Write-Host "and the value 10 is suggested if more than one adapter was assigned a default route."
+Write-Host
+Write-Host "Current default route(s):"
+Try { $(Find-NetRoute -RemoteIPAddress 0.0.0.0 -ErrorAction Stop )[1] | fl InterfaceAlias, InterfaceMetric }
+Catch { Write-Warning "None! You may have to wait a few moments ;-)" }
+
+ncpa.cpl
+
+Pause
+
--- README.md
+++ README.md
@@ -1,2 +1,154 @@
-# ConfigureVSwitch
- connects the Hyper-V management Windows OS to multiple VLANs
+# About CreateVSwitch
+
+An external virtual switch allows the management operating system to share an external network. The Hyper-V Virtual Switch Manager GUI interface allows a single share.
+
+This script connects to multiple VLANs the management Windows OS of any workstation capable of running Hyper-V. Each virtual interface can be configured independently, much like a port on a physical switch.
+
+The final network configuration is left to the user's creativity ;-)
+
+The script relies on a configuration file enumerating the desired VLANs in the following format:
+
+<VLAN_ID>	<VLAN_Description>	[(<Subnet>/<SubnetWidth>;<GatewayIP>)[,(<Subnet>/<SubnetWidth>;<GatewayIP>)[....]]]
+
+where
+- <VLAN_ID> is the VLAN identifier to assign to a "port" of the virtual switch
+- <VLAN_Description> is a name that will be assigned to this "port"
+- [...] is an optional list of network routes that will be accessible from this "port".
+Each tuple is enclosed in () and is composed of a subnet specification and a gateway IP that will be accessible from this port. These are delimited by a ; within the tuple and a comma separated list of tuples can be specified. Although both IPv4 and IPv6 subnets and addresses can be specified, the parsing here is minimal and some errors will only get caught at runtime.
+
+No white space is allowed in any of the parameters. Comments can be inserted using the usual # caracter.
+
+Example configuration file:
+
+```
+	# VLAN 201 reserved for support staff
+	201	Support	(10.30.0.0/16;10.200.200.241),(192.168.0.0/16;10.200.200.241)
+	# Projects
+	400	Project_400
+	401	Project_401
+	# Default untagged entry
+	0	Untagged
+```
+
+In the above example, the gateway 10.200.200.241 must be accessible using the effective routing table when the NIC configured for VLAN 201 is enabled. When this NIC is disabled, the two subnets in the example are not reachable.
+
+------
+>**Caution:**	This script requires **elevated** execution privileges.
+
+Quoting from Microsoft's "about_Execution_Policies" : "PowerShell's
+execution policy is a safety feature that controls the conditions
+under which PowerShell loads configuration files and runs scripts."
+
+Use any configuration that is the equivalent of the
+following commnand executed from an elevated PowerShell prompt:
+
+			Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted
+------
+
+## Operation:
+
+You must run PowerShell as an administrator. Invoke the script (relative path not specified here).
+Script parameters are:
+
+	ConfigueVSwitch.ps1 -DefaultManagementSwitch=<string> -DefaultPhysicalPortName=<string>
+
+where:
+
+- -DefaultManagementSwitch:     is the virtual switch name as will be displayed in Hyper-V. There default value is "ManagementSwitch".
+
+- -DefaultPhysicalPortName:     is the name that will be assigned to the external NIC. There default value is "$Switch".
+								When the script deletes the management switch (see below), the adapter is renamed "Ethernet XX-YY-ZZ" where XX-YY-ZZ are the last 6 digits of the MAC address.
+
+The script can be invoked with a righ-click "Execute with PowerShell" and will attempt privilege escalation if possible.
+
+On entry, the user must select the configuration file that will be used. If no file is selected, te script defaults to a simple untagged configuration.
+
+The script displays a grid-view of the available VLANs and the user MUST select the desired VLANs in this instance. If the user aborts at this point, the virtual management switch is deleted.
+
+The script disables ALL the VLANs that it configures. It will launch the Network Connections Control Panel where you can configure and enable each virtual adapter to your liking.
+
+**Caution:** Enabling multiple adapters with a default route can result in loss of connectivity to your system. Please review the adapter metric for desired default route: it is typically 25 for a Gigabit adapter and the value 10 is suggested if more than one adapter was assigned a default route (using a DHCP server, for example).
+
+**Caution:** You can remotely configure a system if you maintain untagged (access) on your trunk. As stated above, all VLANs configured by this script are disabled on exit and you must have an alternte way to reach the system if you want to avoid loss of connectivity.
+
+## Sample console output based on the example configuration
+
+```
+  
+Create multiple VLANs for Windows 10/11
+---------------------------------------
+
+        (This script is based on an idea from Alexander Taeffner
+		see https://taeffner.net/2022/04/multiple-vlans-windows-10-11-onboard-tools-hyper
+
+PowerShell version:  . 5.1.22621.2506
+
+Status of Hyper-V services on this system:                                                                              
+------------------------------------------
+
+DisplayName                                                    FeatureName                               State          
+-----------                                                    -----------                               -----          
+Hyper-V                                                        Microsoft-Hyper-V-All                   Enabled          
+Plateforme Hyper-V                                             Microsoft-Hyper-V                       Enabled          
+Outils d’administration Hyper-V                                Microsoft-Hyper-V-Tools-All             Enabled          
+Module Hyper-V pour Windows PowerShell                         Microsoft-Hyper-V-Management-PowerShell Enabled          
+Hyperviseur Hyper-V                                            Microsoft-Hyper-V-Hypervisor            Enabled          
+Services Hyper-V                                               Microsoft-Hyper-V-Services              Enabled          
+Outils de gestion de l’interface graphique utilisateur Hyper-V Microsoft-Hyper-V-Management-Clients    Enabled
+
+WARNING : Note: This script will not attempt ANY recovery in case of errors.
+
+Configuration selected:
+-----------------------
+
+ ID Name        Routes
+ -- ----        ------
+201 Support     (10.30.0.0/16;10.200.200.241),(192.168.0.0/16;10.200.200.241)
+400 Project_400
+401 Project_401
+  0 Untagged
+
+
+
+VLANs for virtual switch : ManagementSwitch (Physical adapter: Ethernet 3C-08-7A )
+----------------------------------------------------------------------------------
+Adding:  201 Support
+Adding:  400 Project_400
+Adding:  401 Project_401
+WARNING : Waiting 5 seconds for the system to identify the new interfaces...
+
+MACAddress        VLANId     Mode PortName
+----------        ------     ---- --------
+00-15-5D-12-1F-62    401   Access Project_401
+00-15-5D-12-1F-60    201   Access Support
+48-21-0B-3C-08-7A      0 Untagged Untagged
+00-15-5D-12-1F-61    400   Access Project_400
+
+
+VLAN  401 Port Project_401 is now disabled.
+VLAN  201 Port Support is now disabled.
+VLAN  400 Port Project_400 is now disabled.
+
+The network adapter has been renamed $Switch.
+
+Routes with a limited lifetime value:
+-------------------------------------
+
+ifIndex DestinationPrefix NextHop      InterfaceAlias   ValidLifetime
+------- ----------------- -------      --------------   -------------
+     22 0.0.0.0/0         192.168.18.1 $Switch Untagged 12:00:00
+
+
+Please review the adapter metric for desired default route. It is typically 25 for a Gigabit adapter
+and the value 10 is suggested if more than one adapter was assigned a default route.
+
+Current default route(s):
+
+
+InterfaceAlias  : $Switch Untagged
+InterfaceMetric : 25
+
+
+Press Enter to continue...:
+
+```
